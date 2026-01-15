d44ce0ea9e2a0bd4665c3b31e767ab8b2d199cf2
shravanishep <shravanishep@gmail.com>
2026-01-15 11:59:26 +0530

    core static analysis for time and space complexity

--- src/ForLoopCounter.java
+++ src/ForLoopCounter.java
@@ -2,8 +2,14 @@ import com.github.javaparser.ast.stmt.ForStmt;
 import com.github.javaparser.ast.stmt.WhileStmt;
 import com.github.javaparser.ast.stmt.DoStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ForLoopCounter extends VoidVisitorAdapter<Void> {
+private String currentMethod = null;
+public Set<String> recursiveMethods = new HashSet<>();
 
     public int currentDepth = 0;
     public int maxDepth = 0;
@@ -39,4 +45,28 @@ public class ForLoopCounter extends VoidVisitorAdapter<Void> {
         super.visit(n, arg);
         exitLoop();
     }
+
+
+
+    @Override
+public void visit(MethodDeclaration n, Void arg) {
+    String previousMethod = currentMethod;
+    currentMethod = n.getNameAsString();
+
+    super.visit(n, arg);
+
+    currentMethod = previousMethod;
+}
+
+
+@Override
+public void visit(MethodCallExpr n, Void arg) {
+    if (currentMethod != null &&
+        n.getNameAsString().equals(currentMethod)) {
+        recursiveMethods.add(currentMethod);
+    }
+    super.visit(n, arg);
+}
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,16 @@ public class Main {
 
         String code = """
             public class Test {
+
+                    static int fact(int n) {
+            if (n == 0) return 1;
+            return n * fact(n - 1);
+        }
                 public static void main(String[] args) {
-                    while (true) {
-    for (int i = 0; i < n; i++) {
-        do {
-            x++;
-        } while (x < n);
-    }
-}
+               
+
+         System.out.println(fact(5));
+
 
                 }
             }
@@ -39,6 +41,18 @@ public class Main {
 
             System.out.println("Max for-loop nesting depth: " + depth);
             System.out.println("Estimated Time Complexity: " + timeComplexity);
+            System.out.println("Recursive methods: " + counter.recursiveMethods);
+            String spaceComplexity;
+
+if (counter.recursiveMethods.isEmpty()) {
+    spaceComplexity = "O(1)";
+} else {
+    spaceComplexity = "O(n)";
+}
+
+System.out.println("Estimated Space Complexity: " + spaceComplexity);
+
+
 
         } catch (Exception e) {
             System.out.println("Invalid Java code");
